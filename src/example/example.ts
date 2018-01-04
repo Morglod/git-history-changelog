e75427f8b0d5121c8fce94151fb9687ca4733c02
morglod <morglod@gmail.com>
2018-01-04 08:36:00 +0300

    dev: example with categories

--- src/example/example.ts
+++ src/example/example.ts
@@ -5,9 +5,18 @@ async function main() {
     const store = await ghc.Store.openOrCreate('./changelog.json', 'autosave');
     const parsedCommits = await ghc.parseUntrackedCommits(store, { path: './' });
     const filtered = await ghc.filterParsedCommits(store, parsedCommits, (x) => {
-        return {
-            'log': x.message.split(';').map(y => y.trim())
+        const messages = x.message.split(';').map(y => y.trim());
+        const categorized: ghc.ChangelogMessages = {};
+        const defaultCategory = 'log';
+
+        for (const msg of messages) {
+            const [ category, text ] = msg.includes(':') ? msg.split(':') : [ defaultCategory, msg ];
+            if (!(category in categorized))
+                categorized[category] = [];
+            categorized[category].push(text.trim());
         }
+
+        return categorized;
     });
     const changelog = await ghc.toChangelog(store, filtered);
     const rendered = ghc.renderChangelog(store, {
